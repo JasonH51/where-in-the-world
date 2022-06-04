3f32a21dcd465f7bd876b655f16870f7cd0b981e
JasonH51 <81194110+JasonH51@users.noreply.github.com>
2022-06-03 23:23:22 -0500

    finished country page and added the ability to click on border countries to change selected country data

--- src/country/CountryPage.jsx
+++ src/country/CountryPage.jsx
@@ -2,10 +2,10 @@ import React, {useContext, useEffect} from 'react';
 import {Row, Col} from 'react-bootstrap';
 import {Data} from '../API/CountryDataAPI';
 import './css/countryPage.css';
-import axios from 'axios';
+import {navigate} from '@reach/router';
 
 const CountryPage = () => {
-  const {selected, getBorders, borders, setBorders} = useContext(Data);
+  const {selected, setSelected, getBorders, borders, setBorders} = useContext(Data);
 
   useEffect(() => {
     try {
@@ -13,87 +13,98 @@ const CountryPage = () => {
         let border = selected?.borders.join(',');
         getBorders(border).then(res => setBorders(res.data));
         console.log(border);
+      } else {
+        setBorders([]);
       }
     } catch (error) {
       console.log(error);
     }
   }, [selected]);
 
-  console.log(selected?.borders);
-  console.log(borders.data);
+  console.log(borders);
 
   return (
-    <Row>
+    <Row className="country_page_main_container">
       <Row>
         <Col>
-          <button>back</button>
+          <button onClick={() => navigate('/')} className="country_page_back_button">
+            Back
+          </button>
         </Col>
       </Row>
-      <Row>
+      <Row className="country-page-content-container">
         <Col xs={12} md={6}>
-          <div className="country-page-img-container">
+          <div>
             <img src={selected.flags.svg} alt="countries flag" className="flag-img" />
           </div>
         </Col>
-        <Col>
-          <Row>
-            <Col>{selected.name}</Col>
+        <Col xs={12} md={6} className="d-flex flex-column justify-content-center pt-5 pt-md-0 ps-md-5">
+          <Row className="mb-3">
+            <Col>
+              <h1 className="fw-bold">{selected.name}</h1>
+            </Col>
           </Row>
           <Row>
             <Col>
               <Row>
-                <p>
+                <p className="country_page_labels">
                   Native Name: <span>{selected.nativeName}</span>
                 </p>
               </Row>
               <Row>
-                <p>
+                <p className="country_page_labels">
                   Population: <span>{selected.population}</span>
                 </p>
               </Row>
               <Row>
-                <p>
+                <p className="country_page_labels">
                   Region: <span>{selected.region}</span>
                 </p>
               </Row>
               <Row>
-                <p>
+                <p className="country_page_labels">
                   Sub Region: <span>{selected.subregion}</span>
                 </p>
               </Row>
               <Row>
-                <p>
+                <p className="country_page_labels">
                   Capital: <span>{selected.capital}</span>
                 </p>
               </Row>
             </Col>
             <Col>
               <Row>
-                <p>
+                <p className="country_page_labels">
                   Top Level Domain: <span>{selected?.topLevelDomain}</span>
                 </p>
               </Row>
               <Row>
-                <p>
+                <p className="country_page_labels">
                   Currencies: <span>{selected.currencies[0].code}</span>
                 </p>
               </Row>
               <Row>
-                <p>
+                <p className="country_page_labels">
                   Languages:{' '}
                   <span>{selected.languages.map(language => language.name).join(', ')}</span>
                 </p>
               </Row>
             </Col>
           </Row>
-          <Row>
+          <Row className="mt-5">
             <Col>
-              <p>
-                Border Countries:{' '}
+              <p className="country_page_labels">Border Countries:{'   '}</p>
+              <div className="country_page_border_container">
                 {borders.map((el, idx) => (
-                  <span key={idx}>{el.name}, </span>
+                  <span
+                    onClick={() => setSelected(el)}
+                    className="country_page_border_pill"
+                    key={idx}
+                  >
+                    {el.name}
+                  </span>
                 ))}{' '}
-              </p>
+              </div>
             </Col>
           </Row>
         </Col>
